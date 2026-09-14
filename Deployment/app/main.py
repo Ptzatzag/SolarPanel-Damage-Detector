4627f657bdcbd3f9a2e7f8402c6b69235b7914ec
Panos Tzatzagos <ptzatzag@gmail.com>
2026-09-14 15:19:22 +0200

    Create api then load the model

--- Deployment/app/main.py
+++ Deployment/app/main.py
@@ -5,29 +5,38 @@ from huggingface_hub import hf_hub_download
 import torch
 from .inference import predict
 from model.maskrcnn import load_model
+from contextlib import asynccontextmanager
+from fastapi import Request
 
 
-# Download weights from HF Hub
-weight_path = hf_hub_download(
-    repo_id="Ptzatzag/solar-panel-detector", 
-    filename="best_modelmult.pth"             
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    weight_path = hf_hub_download(
+        repo_id="Ptzatzag/solar-panel-detector",
+        filename="best_modelmult.pth",
+    )
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    app.state.model = load_model(weight_path, device)
+
+    yield  
+
+    del app.state.model 
+
+
+app = FastAPI(
+    title="Solar Panel Damage Detection",
+    lifespan=lifespan,
 )
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model = load_model(weight_path, device)
-
-
-app = FastAPI(title="Solar Panel Damage Detection")
 
 @app.get('/')
 def health_check():
     return {'status': 'ok'}
 
 @app.post('/predict')
-async def predict_damage(file: UploadFile = File(...)):
+async def predict_damage(request: Request, file: UploadFile = File(...)):
+    
     image_bytes = await file.read()
     image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
-    
-    
-    result = predict(image, model)
+    result = predict(image, request.app.state.model)
     return result
